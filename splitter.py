215013f81fee6c2bf05560ccee9affcf8fb473be
Robert Crandall <robert.crandall@gmail.com>
2020-05-27 13:46:33 -0700

    Create splitter.py

---
+++ splitter.py
@@ -0,0 +1,499 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import subprocess
+import sys
+import datetime
+import time
+import xml.etree.ElementTree
+import re
+import logging
+import glob
+import csv
+import pandas
+from pathlib import Path
+
+from clint.textui import colored, progress
+import eyed3
+from eyed3.utils import art
+from mutagen.mp3 import MP3
+
+logger = logging.getLogger(__file__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARN)
+logger.addHandler(ch)
+logger.setLevel(logging.INFO)
+
+#### OPTIONS ######
+output_debug_csvs = False
+title_include_track_number = False # True: 01/09 - Title; False: Title
+overwrite_mp3tags = True # Write mp3 tags, even if ffmpeg doesn't process the file
+###################
+
+__version__ = '0.1.0'  
+
+MARKER_TIMESTAMP_MMSS = r'(?P<min>[0-9]+):(?P<sec>[0-9]+)\.(?P<ms>[0-9]+)'
+MARKER_TIMESTAMP_HHMMSS = r'(?P<hr>[0-9]+):(?P<min>[0-9]+):(?P<sec>[0-9]+)\.(?P<ms>[0-9]+)'
+
+title = ""
+author = ""
+year = 0
+starttime_offset = 0
+
+def hhmmss_to_seconds(marker_timestamp):
+    """
+    Convert HH MM SS (FFF) timestamps to number of seconds
+    """
+    timestamp = None
+    for r in ('%M:%S.%f', '%H:%M:%S.%f'):
+        try:
+            timestamp = time.strptime(marker_timestamp, r)
+            ts = datetime.timedelta(
+                hours=timestamp.tm_hour, minutes=timestamp.tm_min, seconds=timestamp.tm_sec)
+            ts_mark = int(ts.total_seconds())
+            break
+        except ValueError:
+            pass
+
+    if not timestamp:
+        # some invalid timestamp string, e.g. 60:15.00
+        mobj = re.match(MARKER_TIMESTAMP_HHMMSS, marker_timestamp)
+        if mobj:
+            ts_mark = int(mobj.group('hr')) * 60 * 60 + \
+                        int(mobj.group('min')) * 60 + \
+                        int(mobj.group('sec')) 
+        else:
+            mobj = re.match(MARKER_TIMESTAMP_MMSS, marker_timestamp)
+            if mobj:
+                ts_mark = int(mobj.group('min')) * 60 + \
+                            int(mobj.group('sec'))
+            else:
+                raise ValueError('Invalid marker timestamp: {}'.format(marker_timestamp))
+    return ts_mark
+
+def convert_time(time_secs, offset):
+    """
+    Convert seconds to HH MM SS (FFF)
+    Offset is how many seconds to subtract from the original value
+    """
+    if time_secs > 0:
+        time_secs = time_secs - offset
+    fraction = int((time_secs % 1) * 1000)
+    seconds = int(time_secs)
+    min, sec = divmod(seconds, 60)
+    hour, min = divmod(min, 60)
+    return f"{hour:02}:{min:02}:{sec:02}.{fraction:03}"
+
+
+def mp3_duration_seconds(filename):
+    # Ref: https://github.com/ping/odmpy/pull/3
+    # returns the length of the mp3 in seconds
+
+    # eyeD3's audio length function:
+    # audiofile.info.time_secs
+    # returns incorrect times due to it's header computation
+    # mutagen does not have this issue
+    audio = MP3(filename)
+    return int(round(audio.info.length))
+
+def checkKey(dict, key): 
+    """
+    Returns true if a dictionary's key is present and not None
+    """
+    if key in dict.keys(): 
+        if dict[key] is None:
+            return False
+        return True
+    return False
+
+def make_safe_filename(s):
+    """
+    Returns string of a filesystem safe name
+    """
+    def safe_char(c):
+        if c.isalnum():
+            return c
+        elif c == ' ' or c == '-' or c == '_':
+            return c
+        else:
+            return ""
+    return "".join(safe_char(c) for c in s).rstrip("")
+
+def verifyTitle(filename):
+    """
+    Reads title/author from MP3, sets title, author, year
+    """
+    global title, author, year
+
+    audiofile = eyed3.load(filename)
+    title = audiofile.tag.album
+    author = audiofile.tag.artist
+
+    print("Read title as: {}".format(title))
+    inputtitle = input("Press enter to accept, over enter new title: ")
+    if len(inputtitle) > 1:
+        title = inputtitle
+
+    print("Read author as: {}".format(author))
+    inputauthor = input("Press enter to accept, over enter new title: ")
+    if len(inputauthor) > 1:
+        author = inputauthor
+
+    year = input("Book Year: ")
+
+def build_chapter_object(part_filename):
+    """
+    Extracts Overdrive Markers from MP3 tag, stores them in list part_markers
+    """
+    global part_markers, track_count, file_merged_start_time
+    audiofile = eyed3.load(part_filename)
+    previous_chapter = "zycndkldms" ## Just a garbage value
+
+    ## Read MP3 metadata
+    for frame in audiofile.tag.frame_set.get(eyed3.id3.frames.USERTEXT_FID, []):
+        if frame.description != 'OverDrive MediaMarkers':
+            continue
+        if frame.text:
+            try:
+                tree = xml.etree.ElementTree.fromstring(frame.text)
+            except UnicodeEncodeError:
+                tree = xml.etree.ElementTree.fromstring(frame.text.encode('ascii', 'ignore').decode('ascii'))
+
+            ## Overdrive can do the following:
+            ## * Store Chapter by itself: "Chapter 1"
+            ## * Split Chapter within a single part file: "Chapter 1", "Chapter 1 (12:01)"
+            ## * Split Chapter across multiple part files, with or without the (12:01) above
+
+            ## chapter_section checks if chapter is split
+            chapter_section = 0
+            for m in tree.iter('Marker'):
+                marker_name = m.find('Name').text.strip()
+                marker_timestamp = m.find('Time').text
+
+                ## Store "Chapter 1" as previous timestamp; "Chapter 1 (12:01) starts with previous chapter"
+                ## New chapter name (should just be "Chapter 1")
+                if not marker_name.startswith(previous_chapter):
+                    previous_chapter = marker_name
+                    chapter_section = 0
+                ## Chapter name is "Chapter 1 (12:01)"
+                else:
+                    chapter_section += 1
+
+                # Save "Chapter 1" as the name, regardless of real name
+                marker_name = previous_chapter
+                ts_mark = hhmmss_to_seconds(marker_timestamp)
+                
+                start_time = ts_mark
+                track_count += 1
+
+                audiopart = {
+                    'filename': part_filename, 
+                    'file_number': track_count,
+                    'chapter_name': marker_name,
+                    'chapter_section': chapter_section,
+                    'start_time': start_time}
+                part_markers.append(audiopart)
+        break
+
+def smooshChapters(part_markers):
+    """
+    Combine chapter parts, get earliest start time and latest end time for each combined
+    chapter part
+    """
+
+    global starttime_offset
+
+    smooshed_chapters = []
+
+    df = pandas.DataFrame(part_markers).sort_values(by=['file_number'])
+    ## Get all unique chapter names. "Chapter 1" in the above example, ignoring "Chapter 1 (12:01)"
+    unique_chapters = df.chapter_name.unique()
+    track_number = 0
+
+    for chapter in unique_chapters:
+        ## Get all parts that match this chapter
+        chapters = df.loc[df['chapter_name'] == chapter]
+
+        split = None
+        splitName = None
+        track_number += 1
+
+        ## Check if this chapter is spread across multiple MP3 files
+        unique_parts = chapters.filename.unique()
+        if len(unique_parts) > 1:
+
+            # Save these files as a temporary Chapter-PartX for now
+            for index, part in enumerate(unique_parts):
+                parts = chapters.loc[chapters['filename'] == part]
+                splitName = u"-Part" + str(index)
+
+                ## If this chapter is split between MP3s, and we're not on the last chapter/part, endtime of (chapter/part) will be the endtime of the current MP3 files
+
+                endtime = None
+                if index < len(unique_parts) - 1:
+                    endtime = convert_time(mp3_duration_seconds(parts.filename.min()), 0)
+
+                smooshed_chapter = {
+                    'filename': parts.filename.min(),
+                    'track_number': track_number,
+                    'chapter_name': chapters.chapter_name.min(),
+                    'split_name': parts.chapter_name.min() + splitName,
+                    'filenumber': parts.file_number.min(),
+                    'start_time': convert_time(parts.start_time.min(), starttime_offset),
+                    'end_time': endtime
+                }
+                smooshed_chapters.append(smooshed_chapter)
+        else:
+            smooshed_chapter = {
+                'filename': chapters.filename.min(),
+                'track_number': track_number,
+                'chapter_name': chapters.chapter_name.min(),
+                'split_name': None,
+                'filenumber': chapters.file_number.min(),
+                'start_time': convert_time(chapters.start_time.min(), starttime_offset),
+                'end_time': None
+            }
+            smooshed_chapters.append(smooshed_chapter)
+
+    ## Sort again, just in case
+    smooshed_chapters = sorted(smooshed_chapters, key = lambda i: i['filenumber'])
+
+    ## Get end time of this chapter
+    for index, chapter in enumerate(smooshed_chapters):
+
+        ## No endtime currently exists
+        if not checkKey(chapter, 'end_time'):
+            ## There are more files. End time is the start time of the next chapter
+            if index < len(smooshed_chapters) - 1:
+                endtime = smooshed_chapters[index+1]["start_time"]
+            ## No more files. End time is the end of the file.
+            else:
+                endtime = convert_time(mp3_duration_seconds(chapter['filename']), 0)
+
+            ## It's possible the grabbed end time (start time of the next chapter) is 0, indicating a new MP3. In that case, the real end time is the end of the current file.
+            if hhmmss_to_seconds(endtime) < 1:
+                endtime = convert_time(mp3_duration_seconds(chapter['filename']), 0)
+            chapter['end_time'] = endtime
+
+    return smooshed_chapters, track_number
+
+def writeMP3Tags(mp3file, chaptertitle, tracknumber):
+    """
+    Write output Mp3 file with title, author, cover
+    """
+    global author, title, cover_bytes, year
+
+    audiofile = eyed3.load(mp3file)
+    audiofile.tag.artist = author
+    audiofile.tag.album = title
+    audiofile.tag.album_artist = author
+    audiofile.tag.title = chaptertitle
+    audiofile.tag.track_num = tracknumber
+    audiofile.tag.recording_date = year
+    audiofile.tag.images.set(
+        art.TO_ID3_ART_TYPES[art.FRONT_COVER][0], cover_bytes, 'image/jpeg', description=u'Cover')
+
+    audiofile.tag.save()
+
+def writeChapterFiles(chapters, totaltracks, basedir):
+    global failed_files
+
+    leadingzeros = 2
+    if totaltracks > 99:
+        leadingzeros = 3
+
+    ## Store items that need reprocessing
+    needsmerging = []
+    for chapter in chapters:
+        ## Set all the names. 
+        ### Store non-split chapters in final directory
+ 
+        foldername = u'{} ({})'.format(title, str(year)) ## Folder to store completed project. I set this as: BookTitle (Year)
+
+        outdir = u'{}/{}'.format(basedir, foldername) ## Making the final folder stay as a subdir of processing directory
+        tempdir = u'{}/{}'.format(basedir, "temp") ## Directory to store temp files
+        outputname = u'{} - {}.mp3'.format(str(chapter['track_number']).zfill(leadingzeros), make_safe_filename(chapter['chapter_name'])) ## MP3 name. I set as Track - Title
+        outputpath = u'{}/{}'.format(outdir, outputname) ## Tells ffmpeg this is the output for this part
+        
+        outputtitle =  u'{}'.format(chapter['chapter_name'])
+        if title_include_track_number:
+            outputtitle =  u'{}/{} - {}'.format(str(chapter['track_number']).zfill(leadingzeros), str(totaltracks).zfill(leadingzeros), chapter['chapter_name']) ## Set title at Track/Total Tracks - Chapter Name. My personal preference.
+        mp3tracknumber = str(chapter['track_number']).zfill(leadingzeros)
+
+        ## Check if this is a split chapter
+        ### Store split chapters in temp directory and save in needsmerging list
+        if checkKey(chapter, 'split_name'):
+            tempoutputname = u'{} - {}.mp3'.format(str(chapter['track_number']).zfill(leadingzeros), make_safe_filename(chapter['split_name'])) ## MP3 name. I set as.\sp Track - Title
+            tempoutputpath = u'{}/{}'.format(tempdir, tempoutputname)
+            needsmerge = {
+                'filenumber': chapter['filenumber'],
+                'track_number': chapter['track_number'],
+                'merge_input': tempoutputpath,
+                'merge_output': outputpath,
+                'outputtitle':  outputtitle,
+                'mp3tracknumber': mp3tracknumber
+            }
+            needsmerging.append(needsmerge)
+            outputpath = tempoutputpath
+
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        if not os.path.exists(tempdir):
+            os.makedirs(tempdir)
+ 
+        writemp3tags = False
+        if overwrite_mp3tags:
+            writemp3tags = True
+        if os.path.isfile(outputpath):
+            logger.info('Already saved "{}"'.format(
+                colored.magenta(outputpath)))
+        else:
+            cmd = [
+                'ffmpeg', '-y',
+                '-i', chapter['filename'],
+                '-loglevel', 'info' if logger.level == logging.DEBUG else 'error', '-stats',
+                '-acodec', 'copy',
+                '-ss', chapter['start_time'],
+                '-to', chapter['end_time'],
+                outputpath]
+            exit_code = subprocess.call(cmd)
+            writemp3tags = True
+
+            if exit_code > 0:
+                print("Failed processing {}".format(outputpath))
+                failed_files.append(chapter)
+                continue
+
+        ## Tag MP3 file
+        if writemp3tags:
+            writeMP3Tags(outputpath, outputtitle, mp3tracknumber)
+        
+    return needsmerging
+        
+def processMerges(chapters):
+    global failed_files, cover_bytes
+
+    ## Group by track number for safety
+    df = pandas.DataFrame(chapters).sort_values(by=['filenumber'])
+    track_numbers = df.track_number.unique()
+
+    for track in track_numbers:
+        tracks = df.loc[df['track_number'] == track]
+
+        ## Send a list of files to concat, along with output name
+        input = tracks.merge_input.tolist()
+        output = tracks.merge_output.min()
+        written, successful = mergemp3s(input, output)
+        if written:
+            writeMP3Tags(output, tracks.outputtitle.min(), tracks.mp3tracknumber.min())
+
+        if not successful:
+            failed_files.append(track)
+
+def mergemp3s(files, output_filename):
+    if os.path.isfile(output_filename):
+        logger.info('Already saved "{}"'.format(
+            colored.magenta(output_filename)))
+        return False, True
+
+    logger.info('Generating "{}"...'.format(
+        colored.magenta(output_filename)))
+
+    # We can't directly generate a m4b here even if specified because eyed3 doesn't support m4b/mp4
+    cmd = [
+        'ffmpeg', '-y',
+        '-hide_banner',
+        '-loglevel', 'info' if logger.level == logging.DEBUG else 'error', '-stats',
+        '-i', 'concat:{}'.format('|'.join(files)),
+        '-acodec', 'copy',
+        output_filename ]
+    exit_code = subprocess.call(cmd)
+    if exit_code == 0:
+        return True, True
+    return False, False
+
+def outputCSV(mydict, outname):
+    with open(outname, 'w', newline='\n', encoding='utf-8') as f:
+        fc = csv.DictWriter(f, 
+                    fieldnames=mydict[0].keys(),
+                    )
+        fc.writeheader()
+        fc.writerows(mydict)
+
+## Setup global variables. I bet this can be improved.
+part_markers = []
+track_count = 0
+file_merged_start_time = 0
+chapter_markers = []
+failed_files = []
+cover_bytes = None
+
+def main():
+    global cover_bytes
+
+    ## Setup the directory that this is working on
+    parser = argparse.ArgumentParser( )
+
+    parser.add_argument(
+        '-d', '--dir', dest='dir', default='.')
+
+    args = parser.parse_args()
+
+    base_directory = args.dir 
+    base_directory = base_directory.rstrip('\\')
+    base_directory = base_directory.rstrip('"')
+
+    base_directory = Path(base_directory)
+    
+    if not os.path.exists(base_directory):
+        print("Please select an existing directory for -d option")
+        sys.exit(0)
+    
+    ## Get all MP3s in current directory
+    files = [f for f in glob.glob("{}/*.mp3".format(base_directory))]
+    files.sort()
+
+    if len(files) == 0:
+        print("No Mp3 files found at that location")
+        sys.exit(0)
+
+    ## Find cover art. Panic if not found currently.
+    coverfile = [f for f in glob.glob("{}/*Cover.jpg".format(base_directory))]
+    cover_filename = coverfile[0]
+
+    with open(cover_filename, 'rb') as f:
+        cover_bytes = f.read()
+
+    ## Get title, author, year. TODO - improve this.
+    verifyTitle(files[0])
+
+    ## Create a list containing each chapter and timestamp for every mp3 file
+    for filename in files:
+        build_chapter_object(filename)
+
+    ## Combine chapter timestamps to have 1 output per chapter
+    cuttimestamps, totaltracks = smooshChapters(part_markers)
+
+    if output_debug_csvs:
+        outputCSV(cuttimestamps, '{}-cuttimestamps.csv'.format(make_safe_filename(title)))
+
+    ## Split MP3s to chapters
+    needsmerging = writeChapterFiles(cuttimestamps, totaltracks, base_directory)
+
+    ## If any chapter spanned MP3s, combine them
+    if len(needsmerging) > 0:
+        if output_debug_csvs:
+            outputCSV(needsmerging, '{}-needsmerging.csv'.format(make_safe_filename(title)))
+        processMerges(needsmerging)
+
+    ## Write any failed files
+    if len(failed_files) > 0:
+        print("------")
+        print("Following are failed files:")
+        for row in failed_files:
+            print(row)
+        outputCSV(failed_files, '{}-failed_files.csv'.format(make_safe_filename(title)))
+
+
+if __name__ == "__main__":
+    main()
